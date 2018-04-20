c4803b71249b6a03ed2d259ca784115553fc5c1c
Mirko Colemberg <slaet@colemberg.ch>
2018-04-20 15:34:38 +0200

    Update getLanguage.ps1

--- getLanguage.ps1
+++ getLanguage.ps1
@@ -1,3 +1,35 @@
+<#
+
+.SYNOPSIS
+
+    Check the installed Language on a Win10 Client
+
+.DESCRIPTION
+
+    This script is to use in a TS for OSD to check the language on a client to get this nin variables to use it later
+    for installing the right Language Pack and set the right Settings after a Win10 Serviceng update on a TS
+
+.PARAMETER 
+
+    NONE
+
+.EXAMPLE
+
+    powershell.exe -ExecutionPolicy Bypass -getLanguage.ps1
+
+.NOTES
+
+    Script name: getLanguage.ps1
+
+    Version:     1.0
+
+    Author:      Mirko Colemberg // baseVISION AG
+
+    DateCreated: 2018-03-28
+
+#>
+
+
 $tsenv = New-Object -ComObject Microsoft.SMS.TSEnvironment
 $cultureInfo = Get-UICulture
 $setLanguage = switch ($cultureInfo.LCID)
